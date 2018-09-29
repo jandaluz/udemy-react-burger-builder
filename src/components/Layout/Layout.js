24fa703e5a3aa70d1649810a339b964bbe5f9519
jandaluz <jamison.andaluz@gmail.com>
2018-09-29 17:06:53 -0400

    added side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,14 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Auxilliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 
 const layout = (props) => {
     return (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,9 @@ const toolbar = (props) => (
 
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo> </Logo>
+        <div>
+            <Logo />
+        </div>
         <nav>
            <NavigationItems />
         </nav>
